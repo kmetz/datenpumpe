828aafecd7ea419b05984a7fbc90a60cd7bde311
Kaspar Metz <k@kjpm.de>
2018-09-24 16:37:33 +0200

    add datenpumpe-server

---
+++ datenpumpe-server/index.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace Datenpumpe\Server;
+
+/**
+ * Generates data views for the Datenpumpe installation.
+ */
+
+
+deliver_random_query_result();
+
+/**
+ * Main function, prints HTML.
+ */
+function deliver_random_query_result() {
+  $queries = $queryData = [];
+  include('./queries.php');
+
+  if (isset($_GET['list'])) {
+    print '<h2>Datenpumpen-Inhalte (' . count($queries) . ')</h2>';
+    print render_query_list($queries);
+  }
+  elseif (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    if (isset($queries[$_GET['id']])) {
+      $queryData = $queries[$_GET['id']];
+    }
+    else {
+      header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+      exit;
+    }
+  }
+  else {
+    $queryData = $queries[array_rand($queries)];
+  }
+
+  $html = '';
+  switch ($queryData['type']) {
+    case 'singleValue':
+    case 'table':
+    case 'images':
+    case 'map':
+      $html = render_content($queryData);
+      break;
+
+    case 'embed':
+      $html = render_embed($queryData);
+      break;
+  }
+
+  if (isset($_GET['show_query'])) {
+    $html = '<pre>' . $queryData['query'] . '</pre>' . $html;
+  }
+
+  // Deliver
+  print $html;
+  exit;
+}
+
+
+function render_query_list($queries) {
+  $html = '<ul>';
+  foreach ($queries as $id => $data) {
+    $html .= '<li><a href="?id=' . $id . '">' . $data['title'] . '</a>' . ' (' . $data['type'] . ')</li>';
+  }
+  $html .= '</ul>';
+
+  return $html;
+}
+
+
+function render_content($queryData) {
+  $endpointUrl = 'https://query.wikidata.org/sparql';
+  $resultJson = file_get_contents($endpointUrl . '?format=json&query=' . urlencode($queryData['query']));
+  $result = json_decode($resultJson);
+
+  #print '<pre>' . $resultJson . '</pre>';
+  #print '<pre>' . print_r($result, TRUE) . '</pre>';
+
+  $html = '<!DOCTYPE html><html lang="en" dir="ltr">';
+  $html .= '<head><link rel="stylesheet" href="data-style.css"></head>';
+  $html .= '<body>';
+  if (isset($queryData['title'])) {
+    $html .= '<h1>' . $queryData['title'] . '</h1>';
+  }
+  $html .= '<div id="content-wrapper">';
+
+  if (!empty($result->results)) {
+    $render_function = 'Datenpumpe\Server\render_content_' . $queryData['type'];
+    $html .= call_user_func($render_function, $queryData, $result);
+  }
+
+  $html .= '</div>'; // #content-wrapper
+  $html .= '</body></html>';
+
+  return $html;
+}
+
+
+function render_content_singleValue($queryData, $result) {
+  $row = $col = 0;
+  $key = $result->head->vars[$col];
+  $value = $result->results->bindings[$row]->{$key};
+  $html = '<div class="single-value">' . format_value($value) . '</div>';
+
+  return $html;
+}
+
+
+function render_content_table($queryData, $result) {
+  $html = '<table>';
+  $html .= '<thead><tr>';
+  foreach ($result->head->vars as $key) {
+    $html .= '<th>' . $key . '</th>';
+  }
+  $html .= '</tr></thead>';
+  $html .= '<tbody>';
+  foreach ($result->results->bindings as $row) {
+    $html .= '<tr>';
+    foreach ($result->head->vars as $key) {
+      $html .= '<td>' . format_value($row->{$key}) . '</td>';
+    }
+    $html .= '</tr>';
+  }
+  $html .= '</tbody>';
+  $html .= '</table>';
+
+  return $html;
+}
+
+
+function render_content_images($queryData, $result) {
+  $html = '<div class="images">';
+  foreach ($result->results->bindings as $row) {
+    $html .= '<div class="image">';
+    if (isset($row->image) && $row->image->type === 'uri') {
+      $html .= '<img src="' . $row->image->value . '?width=150">';
+    }
+    foreach ($result->head->vars as $key) {
+      if ($key === 'image') {
+        continue;
+      }
+      $html .= '<p>' . format_value($row->{$key}) . '</p>';
+    }
+    $html .= '</div>'; // .image
+  }
+  $html .= '</div>'; // .images
+
+  return $html;
+}
+
+
+function render_content_map($queryData, $result) {
+
+  $geodataKey = FALSE;
+  $layers = [];
+  foreach ($result->results->bindings[0] as $key => $value) {
+    if ($value->datatype === 'http://www.opengis.net/ont/geosparql#wktLiteral') {
+      $geodataKey = $key;
+      break;
+    }
+  }
+  if ($geodataKey) {
+    foreach ($result->results->bindings as $row) {
+      $layer = !empty($row->layer->value) ? $row->layer->value : '0';
+      $layers[$layer][] = $row->{$geodataKey}->value;
+    }
+  }
+  $layers = array_values($layers);
+  $layersJson = json_encode($layers);
+
+  $html = '<link rel="stylesheet" href="vendor/leaflet.css"/>';
+  $html .= '<script src="vendor/leaflet.js" type="text/javascript"></script>';
+  $html .= '<script src="vendor/wicket.js" type="text/javascript"></script>';
+  $html .= '<script src="vendor/wicket-leaflet.js" type="text/javascript"></script>';
+  $html .= '<div id="map"></div>';
+
+  $html .= <<<EOS
+<script type="text/javascript">
+let map = L.map('map', {
+    preferCanvas: true
+});
+map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+if (map.tap) map.tap.disable();
+
+let tileLayer = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    id: 'base',
+}).addTo(map);
+
+let layerColors = [
+  '#009ee0',
+  '#e2007a',
+  '#97b314',
+  '#f29400',
+  '#622181',
+  
+  '#e04700',
+  '#79e200',
+  '#b31496',
+  '#3400f2',
+  '#817121',
+];
+
+let wktLayers = $layersJson;
+
+let markerGroup = new L.FeatureGroup();
+
+let wicket = new Wkt.Wkt();
+let geojson = {};
+for (let layer in wktLayers) {
+  let color = layer < layerColors.length ? layerColors[layer] : layerColors[layerColors.length];
+  for (let item of wktLayers[layer]) {
+    wicket.read(item);
+    geojson = wicket.toJson();
+    let feature = {};
+    if (geojson.type === 'Point') {
+      feature = L.circleMarker([geojson.coordinates[1], geojson.coordinates[0]], {
+        color: color,
+        radius: 5
+      });
+    }
+    else {
+      feature = wicket.toObject({});  
+    }
+    markerGroup.addLayer(feature);
+  }
+}
+markerGroup.addTo(map);
+map.fitBounds(markerGroup.getBounds());
+</script>
+EOS;
+  return $html;
+}
+
+
+function format_value($value) {
+  if ($value->type === 'literal') {
+    switch ($value->datatype) {
+      case 'http://www.w3.org/2001/XMLSchema#integer':
+        return '<span class="number">'
+          . number_format(intval($value->value), 0, ',', strlen($value->value) > 4 ? '.' : '')
+          . '</span>';
+
+      case 'http://www.w3.org/2001/XMLSchema#decimal':
+        return '<span class="number">'
+          . number_format(floatval($value->value), 0, ',', '.')
+          . '</span>';
+
+      case 'http://www.w3.org/2001/XMLSchema#double':
+        return '<span class="number">'
+          . number_format(floatval($value->value), 4, ',', '.')
+          . '</span>';
+    }
+  }
+  if ($value->type === 'uri') {
+    if (strpos($value->value, 'http://www.wikidata.org/entity/') === 0) {
+      return '<span class="entity">' . end(explode('/', $value->value)) . '</span>';
+    }
+
+  }
+
+  return $value->value;
+}
+
+
+/**
+ * Loads a query on query.wikidata.org/embed, modifies and returns the result.
+ * Quick&dirty way to use rendered output from query.wikidata.org.
+ *
+ * @param array $queryData
+ * @return string Resulting HTML.
+ */
+function render_embed($queryData) {
+  // Alternative iframe method:
+  // $html = '<iframe src="'.$queryURL.'" width="1024" height="876"></iframe>';
+
+  $wikidataURL = 'https://query.wikidata.org';
+  $queryURL = $wikidataURL . '/embed.html';
+
+  // Fetch embed html
+  $html = file_get_contents($queryURL);
+  // replace ressource URLs (make them absolute)
+  $html = preg_replace('/(href|src)="\/?((?!http).*?)"/', '${1}=' . $wikidataURL . '/${2}', $html);
+  // Put SPARQL query into URL hash for javascript to render correctly
+  $headAppend = '<script type="text/javascript">window.location.hash = "#' . rawurlencode(trim($queryData['query'])) . '";</script>';
+  // Add our stylesheet
+  $headAppend .= '<link rel="stylesheet" href="data-style.css">';
+  $html = str_replace('</head>', $headAppend . '</head>', $html);
+  // Add title
+  $bodyPrepend = '<h1><span>' . $queryData['title'] . '</span></h1>';
+  $html = str_replace('<body>', '<body>' . $bodyPrepend, $html);
+  // Absolute URL
+  $bodyAppend = '<script type="text/javascript">CONFIG.api.sparql.uri = "https://query.wikidata.org/sparql"</script>';
+  $html = str_replace('</body>', $bodyAppend . '</body>', $html);
+
+  return $html;
+}
---
+++ datenpumpe-server/queries.php
@@ -0,0 +1,666 @@
+<?php
+
+namespace Datenpumpe\Server;
+
+/**
+ * Type can be:
+ * - singleValue
+ *   A big number or text value (first row/column of results).
+ *
+ * - table
+ *   A table of data.
+ *
+ * - map
+ *   A map with layer support (use ?layer in SELECT).
+ *
+ * - images
+ *   Image grid with text captions, use ?image in SELECT for the image URL.
+ *
+ * - embed
+ *   Shows any output dircetly from query.wikimedia.org, make shure to include
+ *   a #defaultView comment (e.g. #defaultView:BubbleChart)
+ */
+
+$queries = [
+
+  0 => [
+    'type' => 'table',
+    'title' => 'Wieviele Päpste hatten wieviele Kinder?',
+    'query' => <<<SPARQL
+SELECT ?Papst (YEAR(?date) AS ?geboren) (COUNT(?child) as ?Kinder) WHERE {
+  ?child wdt:P22 ?father.
+  ?father wdt:P31 wd:Q5;
+  wdt:P39 wd:Q19546;
+  wdt:P569 ?date.
+  OPTIONAL { ?father wdt:P18 ?picture. }
+  ?father rdfs:label ?Papst.
+  FILTER(LANG(?Papst) = 'de').
+}
+GROUP BY ?Papst ?date
+ORDER BY DESC(?Kinder) ?Papst
+SPARQL
+  ],
+
+
+  1 => [
+    'type' => 'images',
+    'title' => 'Welche Nationalflaggen zeigen Sterne?',
+    'query' => <<<SPARQL
+SELECT DISTINCT ?flag ?flagLabel (MIN(?images) AS ?image) WHERE {
+  ?flag wdt:P31/wdt:P279* wd:Q186516;
+  wdt:P180 ?depicted.
+  ?flag wdt:P18 ?images.
+  VALUES ?astronomical { wd:Q8928 wd:Q523 } ?depicted wdt:P31?/wdt:P279* ?astronomical.
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "de".
+    ?flag rdfs:label ?flagLabel.
+    ?depicted rdfs:label ?depictedLabel.
+  }
+}
+GROUP BY ?flag ?flagLabel
+ORDER BY MD5(CONCAT(STR(NOW()), STR(?flag)))
+SPARQL
+  ],
+
+
+  2 => [
+    'type' => 'map',
+    'title' => 'Nur 12 internationale Flughäfen sind nach Frauen benannt',
+    'query' => <<<SPARQL
+SELECT ?airport ?airportLabel ?location ?person ?personLabel (?gender AS ?layer) WHERE {
+  ?airport wdt:P31/wdt:P279* wd:Q644371;
+            wdt:P625 ?location;
+            wdt:P138 ?person.
+  ?person wdt:P31 wd:Q5.
+  OPTIONAL {
+    ?person wdt:P21/wdt:P279* wd:Q6581072.
+    BIND("female"@en AS ?female)
+  }
+  OPTIONAL {
+    ?person wdt:P21/wdt:P279* wd:Q6581097.
+    BIND("male"@en AS ?male)
+  }
+  OPTIONAL {
+    ?person wdt:P21 ?gender.
+      MINUS { ?gender wdt:P279* wd:Q6581097. }
+    MINUS { ?gender wdt:P279* wd:Q6581072. }
+    FILTER(!ISBLANK(?gender))
+    BIND("other"@en AS ?other)
+  }
+  BIND(COALESCE(?female, ?male, ?other, "unknown"@en) AS ?gender)
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+SPARQL
+  ],
+
+
+  3 => [
+    'type' => 'map',
+    'title' => 'Nach Frauen benannte Hochschulen',
+    'query' => <<<SPARQL
+SELECT ?airport ?airportLabel ?location ?person ?personLabel (?gender AS ?layer) WHERE {
+  ?airport wdt:P31/wdt:P279* wd:Q38723;
+            wdt:P625 ?location;
+            wdt:P138 ?person.
+  ?person wdt:P31 wd:Q5.
+  OPTIONAL {
+    ?person wdt:P21/wdt:P279* wd:Q6581097.
+    BIND("male"@en AS ?male)
+  }
+  OPTIONAL {
+    ?person wdt:P21/wdt:P279* wd:Q6581072.
+    BIND("female"@en AS ?female)
+  }
+  OPTIONAL {
+    ?person wdt:P21 ?gender.
+      MINUS { ?gender wdt:P279* wd:Q6581097. }
+    MINUS { ?gender wdt:P279* wd:Q6581072. }
+    FILTER(!ISBLANK(?gender))
+    BIND("other"@en AS ?other)
+  }
+  BIND(COALESCE(?female, ?male, ?other, "unknown"@en) AS ?gender)
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+SPARQL
+  ],
+
+
+  4 => [
+    'type' => 'embed',
+    'title' => 'Meistvererbte Berufe',
+    'query' => <<<SPARQL
+#defaultView:BubbleChart
+SELECT ?occupation ?occupationLabel ?count WITH {
+  SELECT ?occupation (COUNT(DISTINCT ?person1) AS ?count) WHERE {
+    wd:Q5 ^wdt:P31 ?person1, ?person2, ?person3, ?person4, ?person5.
+    ?person1 wdt:P22|wdt:P25 ?person2.
+    ?person2 wdt:P22|wdt:P25 ?person3.
+    ?person3 wdt:P22|wdt:P25 ?person4.
+    ?person4 wdt:P22|wdt:P25 ?person5.
+    
+    ?occupation ^wdt:P106 ?person1, ?person2, ?person3, ?person4, ?person5.
+  }
+  GROUP BY ?occupation ?occupationLabel
+} AS %results WHERE {
+  INCLUDE %results  .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+SPARQL
+  ],
+
+
+  5 => [
+    'type' => 'images',
+    'title' => 'Auswahl früher Kunst von Frauen',
+    'query' => <<<SPARQL
+#defaultView:ImageGrid
+SELECT ?image ?artistLabel ?artLabel ?date WHERE {
+  {
+    SELECT (SAMPLE(?image) AS ?image) ?artist ?art (YEAR(MIN(?date)) AS ?date) WHERE {
+    ?art wdt:P170 ?artist;
+           wdt:P18 ?image;
+           wdt:P571 ?date.
+      ?artist wdt:P31 wd:Q5.
+      { ?artist wdt:P21 wd:Q6581072. } UNION { ?artist wdt:P21 wd:Q1052281. } # using ?artist wdt:P21/wdt:P279? wd:Q6581072 instead would be nice, but is unfortunately too inefficient
+    }
+    GROUP BY ?artist ?art
+    ORDER BY ASC(?date)
+    LIMIT 20
+  }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+ORDER BY MD5(CONCAT(STR(NOW()), STR(?art))) # simulate random order – ORDER BY RAND() doesn’t have the desired effect
+LIMIT 20
+SPARQL
+  ],
+
+
+  6 => [
+    'type' => 'map',
+    'title' => 'Auf "ing" endende Ortschaften in Deutschland',
+    'query' => <<<SPARQL
+#defaultView:Map
+SELECT ?item ?name ?itemCoords WHERE {
+  ?item wdt:P31 wd:Q262166;
+        rdfs:label ?name.
+  FILTER(LANG(?name) = "de")
+  FILTER(STRENDS(?name, "ing"@de))
+  ?item wdt:P625 ?itemCoords.
+}
+SPARQL
+  ],
+
+
+  7 => [
+    'type' => 'map',
+    'title' => 'Auf "stedt" endende Ortschaften in Deutschland',
+    'query' => <<<SPARQL
+#defaultView:Map
+SELECT ?item ?name ?itemCoords WHERE {
+  ?item wdt:P31 wd:Q262166;
+        rdfs:label ?name.
+  FILTER(LANG(?name) = "de")
+  FILTER(STRENDS(?name, "stedt"@de))
+  ?item wdt:P625 ?itemCoords.
+}
+SPARQL
+  ],
+
+
+  8 => [
+    'type' => 'map',
+    'title' => 'Mit "a" endende Ortschaften in Deutschland',
+    'query' => <<<SPARQL
+#defaultView:Map
+SELECT ?item ?name ?itemCoords WHERE {
+  ?item wdt:P31 wd:Q262166;
+        rdfs:label ?name.
+  FILTER(LANG(?name) = "de")
+  FILTER(STRENDS(?name, "a"@de))
+  ?item wdt:P625 ?itemCoords.
+}
+SPARQL
+  ],
+
+
+  9 => [
+    'type' => 'singleValue',
+    'title' => 'Wie alt sind Mitglieder des 19. Bundestags durchschnittlich?',
+    'query' => <<<SPARQL
+SELECT (AVG(?age) AS ?avgAge) WHERE {
+?mdb wdt:P31 wd:Q5;
+     p:P39 [
+ps:P39 wd:Q1939555;
+       pq:P2937 wd:Q30579723
+     ];
+     wdt:P569 ?dob.
+  BIND(FLOOR((NOW() - ?dob)/365.2425) AS ?age)
+}
+SPARQL
+  ],
+
+
+  10 => [
+    'type' => 'map',
+    'title' => 'Sprachverteilung in der Schweiz',
+    'query' => <<<SPARQL
+#defaultView:Map
+  SELECT ?item (SAMPLE(?title) AS ?itemLabel) (SAMPLE(?location) AS ?location) (SAMPLE(?language) AS ?layer)
+WITH {
+  SELECT * WHERE {
+    wd:Q39 p:P1332/psv:P1332/wikibase:geoLatitude ?n;
+           p:P1333/psv:P1333/wikibase:geoLatitude ?s;
+           p:P1334/psv:P1334/wikibase:geoLongitude ?e;
+           p:P1335/psv:P1335/wikibase:geoLongitude ?w.
+  }
+} AS %switzerlandBoundingBox
+WITH {
+  SELECT ?item ?location WHERE {
+    ?item wdt:P17 wd:Q39;
+          wdt:P625 ?location.
+      # filter out some stray results that have country Switzerland but coordinates outside it (e. g. rivers)
+      INCLUDE %switzerlandBoundingBox.
+      BIND(geof:latitude(?location) AS ?lat)
+    BIND(geof:longitude(?location) AS ?lon)
+    FILTER(?s <= ?lat && ?lat <= ?n &&
+           ?w <= ?lon && ?lon <= ?e)
+  }
+} AS %swissItems
+WHERE {
+  INCLUDE %swissItems.
+  VALUES ?wiki { <https://de.wikipedia.org/> <https://fr.wikipedia.org/> <https://it.wikipedia.org/> <https://rm.wikipedia.org/> }
+    ?article a schema:Article;
+           schema:about ?item;
+           schema:isPartOf ?wiki;
+           schema:inLanguage ?language;
+           schema:name ?title.
+}
+GROUP BY ?item
+    HAVING(COUNT(DISTINCT ?wiki) = 1)
+SPARQL
+  ],
+
+
+  11 => [
+    'type' => 'embed',
+    'title' => 'Anteil von Frauen und Johns im britischen Parlament',
+    'query' => <<<SPARQL
+# UK parliaments with count of Johns and count of women
+#defaultView:LineChart
+SELECT ?date ?count ?label WITH {
+  # all MPs with their associated parliament
+  SELECT DISTINCT ?mp ?parliament WHERE {
+    ?mp wdt:P31 wd:Q5.
+    {
+      # new data model: position held – Member of the nth Parliament of the United Kingdom
+      ?mp p:P39/ps:P39 ?position.
+      ?position wdt:P279 wd:Q16707842;
+                p:P279/pq:P2937|wdt:P2937 ?parliament.
+    } UNION {
+      # old data model: member of – nth Parliament of the United Kingdom
+      ?mp p:P463/ps:P463 ?parliament.
+      ?parliament wdt:P31 wd:Q21094819.
+    }
+  }
+} AS %MPsWithParliament WITH {
+  # count of Johns in each parliament
+  SELECT ?parliament (COUNT(DISTINCT ?mp) AS ?johns) WHERE {
+    INCLUDE %MPsWithParliament.
+    ?mp wdt:P735 wd:Q4925477.
+  }
+  GROUP BY ?parliament
+} AS %johns WITH {
+  # count of women in each parliament
+  SELECT ?parliament (COUNT(DISTINCT ?mp) AS ?women) WHERE {
+    INCLUDE %MPsWithParliament.
+    ?mp wdt:P21/wdt:P279* wd:Q6581072. # (P279*: include transgender women – none yet, but will probably happen in the future)
+  }
+  GROUP BY ?parliament
+} AS %women WHERE {
+  # get ?johns and ?women
+  INCLUDE %johns.
+  INCLUDE %women.
+  # fan out single result
+  # ?parliament ?johns ?women
+  # into two results
+  # ?parliament "Johns" ?johns
+  # ?parliament "women" ?women
+  # so the line chart works
+  VALUES ?label { "Johns"@en "women"@en }
+  BIND(IF(?label = "Johns"@en, ?johns, ?women) AS ?count)
+  # get parliament date for the chart
+  ?parliament wdt:P571|wdt:P580 ?date
+}
+SPARQL
+  ],
+
+
+  12 => [
+    'type' => 'table',
+    'title' => 'Frauenanteil in fiktiven Universen',
+    'query' => <<<SPARQL
+# ratio of female characters per fictional universe
+# (assuming that any item with “from fictional universe” and “sex or gender” is a fictional character)
+SELECT ?universeLabel (?percentString AS ?Anteil) (?females AS ?Frauen) (?total as ?Alle) WHERE { # add ?percent if you want to sort in the table
+  {
+    SELECT ?universe (SUM(?female) AS ?females) (COUNT(*) AS ?total) WHERE {
+    ?character wdt:P1080 ?universe;
+                 wdt:P21 ?gender.
+      BIND(IF(?gender IN (wd:Q6581072, wd:Q1052281), 1, 0) AS ?female)
+    }
+    GROUP BY ?universe
+  }
+  BIND(?females/?total AS ?ratio)
+  BIND(100*?ratio AS ?percent)
+  BIND(CONCAT(SUBSTR(STR(?percent), 1, 5), "%") AS ?percentString)
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+ORDER BY DESC(?total)
+LIMIT 20
+SPARQL
+  ],
+
+
+  13 => [
+    'type' => 'table',
+    'title' => 'Daten zu bekannten Katzen',
+    'query' => <<<SPARQL
+SELECT ?cat ?catLabel ?catDescription (YEAR(?date) as ?born) (GROUP_CONCAT(?ownerLabel; SEPARATOR=", ") AS ?owner)
+WHERE {
+  ?cat wdt:P31 wd:Q146 .
+  ?cat wdt:P569 ?date .
+  ?cat wdt:P127 ?ownerItem.
+  ?ownerItem rdfs:label ?ownerLabel.
+  FILTER(LANG(?ownerLabel) = 'en').
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en" }
+}
+GROUP BY ?cat ?catLabel ?catDescription ?date
+ORDER BY MD5(CONCAT(STR(NOW()), STR(?item)))
+LIMIT 13
+SPARQL
+  ],
+
+
+  14 => [
+    'type' => 'table',
+    'title' => 'Welche großen Städte liegen am tiefsten?',
+    'query' => <<<SPARQL
+SELECT ?cityLabel ?countryLabel ?altitude ?population WHERE
+{
+  ?city wdt:P31/wdt:P279* wd:Q1549591 ;
+    wdt:P2044 ?altitude ;
+    wdt:P17 ?country ;
+    wdt:P1082 ?population .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en" }
+}
+ORDER BY ?altitude ?cityLabel
+LIMIT 19
+SPARQL
+  ],
+
+
+  15 => [
+    'type' => 'table',
+    'title' => 'Welche großen Städte liegen am höchsten?',
+    'query' => <<<SPARQL
+SELECT ?cityLabel ?countryLabel ?altitude ?population WHERE
+{
+  ?city wdt:P31/wdt:P279* wd:Q1549591 ;
+    wdt:P2044 ?altitude ;
+    wdt:P17 ?country ;
+    wdt:P1082 ?population .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en" }
+}
+ORDER BY DESC(?altitude) ?cityLabel
+LIMIT 19
+SPARQL
+  ],
+
+
+  16 => [
+    'type' => 'map',
+    'title' => 'Kohlekraftwerke in Deutschland',
+    'query' => <<<SPARQL
+#defaultView:Map
+SELECT DISTINCT ?item ?itemLabel ?image ?coor
+  WHERE {
+  ?item wdt:P31/wdt:P279* wd:Q6558431;
+      ?range wd:Q183;
+      wdt:P625 ?coor.
+  FILTER NOT EXISTS { ?item wdt:P576 ?closed. }
+  FILTER NOT EXISTS { ?item wdt:P582 ?enddate. }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "de,en". }
+}
+SPARQL
+  ],
+
+
+  17 => [
+    'type' => 'map',
+    'title' => 'Offshore-Windparks in Europa',
+    'query' => <<<SPARQL
+#defaultView:Map
+SELECT DISTINCT ?item ?coor
+  WHERE {
+  ?item wdt:P31/wdt:P279* wd:Q1357601 .
+  wd:Q46 wdt:P625 ?europeLoc .
+  SERVICE wikibase:around {
+    ?item wdt:P625 ?coor .
+    ?place wdt:P625 ?location .
+    bd:serviceParam wikibase:center ?europeLoc .
+    bd:serviceParam wikibase:radius "4000" .
+  } .
+}
+SPARQL
+  ],
+
+
+  18 => [
+    'type' => 'embed',
+    'title' => 'Tragezeiten von Säugetieren',
+    'query' => <<<SPARQL
+# Average gestation period of genera, color-coded by order
+#defaultView:BubbleChart
+    SELECT ?genus (AVG(?period) AS ?period) (SAMPLE(?label) AS ?genusLabel) (SAMPLE(?rgb) AS ?rgb)
+WHERE
+{
+  {
+    BIND("[AUTO_LANGUAGE]" AS ?language)
+    # find species with gestation period normalized to seconds
+    ?species p:P3063/psn:P3063/wikibase:quantityAmount ?periodSeconds.
+  # convert to days
+  BIND(?periodSeconds/(60*60*24) AS ?period)
+    # find genus
+    ?species wdt:P171* ?genus.
+    ?genus wdt:P105 wd:Q34740.
+  # find a good label – trivial name, else label, else “<no name>”
+  OPTIONAL {
+    ?genus wdt:P1843 ?trivialName.
+    FILTER(LANG(?trivialName) = ?language)
+    }
+    OPTIONAL {
+    ?genus rdfs:label ?genusLabel.
+    FILTER(LANG(?genusLabel) = ?language)
+    }
+    BIND(COALESCE(?trivialName, ?genusLabel, "<kein Name>"@de) AS ?label)
+    # find order
+    ?genus wdt:P171* ?order.
+    ?order wdt:P105 wd:Q36602.
+  # choose “random but deterministic” color per order; you can play around with "-" (can be any string) to find a pleasing resulting color distribution
+  BIND(UCASE(SUBSTR(SHA256(CONCAT("-", STR(?order))), 1, 6)) AS ?rgb)
+  }
+  UNION
+  {
+    # add scale
+    VALUES (?genus ?period ?label) {
+    (wd:Q23387 7 "Woche"@de)
+      (wd:Q5151 30.436875 "Monat"@de)
+      (wd:Q1643308 91.310625 "3 Monate"@de)
+      (wd:Q2269240 182.62125 "6 Monate"@de)
+      (wd:Q577 365.2425 "Jahr"@de)
+    }
+    BIND("CCCCCC" AS ?rgb)
+  }
+}
+GROUP BY ?genus
+SPARQL
+  ],
+
+  19 => [
+    'type' => 'embed',
+    'title' => 'Kinder von Dschingis Khan',
+    'query' => <<<SPARQL
+#Children of Genghis Khan
+#defaultView:Graph
+  PREFIX gas: <http://www.bigdata.com/rdf/gas#>
+
+SELECT ?item ?itemLabel ?pic ?linkTo
+  WHERE {
+  SERVICE gas:service {
+    gas:program gas:gasClass "com.bigdata.rdf.graph.analytics.SSSP" ;
+                gas:in wd:Q720 ;
+                gas:traversalDirection "Forward" ;
+                gas:out ?item ;
+                gas:out1 ?depth ;
+
+                gas:maxIterations 3 ;
+                gas:linkType wdt:P40 .
+  }
+  OPTIONAL { ?item wdt:P40 ?linkTo }
+  OPTIONAL { ?item wdt:P18 ?pic }
+  SERVICE wikibase:label {bd:serviceParam wikibase:language "de,en" }
+}
+SPARQL
+  ],
+
+
+  20 => [
+    'type' => 'images',
+    'title' => 'Benannt nach Nelson Mandela',
+    'query' => <<<SPARQL
+SELECT ?label ?image ?loc WHERE {
+  ?item wdt:P138 wd:Q8023.
+  ?item wdt:P18 ?image.
+  ?item wdt:P131 ?location.
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "de,en" . 
+    ?item rdfs:label ?label.
+    ?location rdfs:label ?loc.
+  }
+}
+ORDER BY MD5(CONCAT(STR(NOW()), STR(?item)))
+SPARQL
+  ],
+
+
+  21 => [
+    'type' => 'map',
+    'title' => 'Wo wurden Raumfahrerinnen und Raumfahrer geboren?',
+    'query' => <<<SPARQL
+SELECT ?astronaut ?coord (?gender AS ?layer) WHERE {
+  ?astronaut ?x1 wd:Q11631;
+             wdt:P31 wd:Q5;
+             wdt:P21 ?gender;
+  wdt:P19 ?birthplace.
+  ?birthplace wdt:P625 ?coord.
+}
+ORDER BY DESC(?gender)
+SPARQL
+  ],
+
+  22 => [
+    'type' => 'map',
+    'title' => 'U-Bahnhöfe in Berlin',
+    'query' => <<<SPARQL
+  SELECT ?station ?stationLabel ?coord (?line AS ?layer)
+WHERE {
+  ?station wdt:P31/wdt:P279* wd:Q928830;
+             wdt:P131 wd:Q64;
+	     wdt:P81 ?line ;
+		 wdt:P625 ?coord .
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],de" }
+}
+SPARQL
+  ],
+
+  23 => [
+    'type' => 'embed',
+    'title' => 'Anzahl der Bahnlinien',
+    'query' => <<<SPARQL
+#defaultView:BubbleChart
+SELECT ?country ?countryLabel (COUNT(?station) AS ?stations)
+WHERE {
+  ?station wdt:P31/wdt:P279* wd:Q15141321;
+             wdt:P17 ?country;
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],de" }
+}
+GROUP BY ?country ?countryLabel
+SPARQL
+  ],
+
+
+  24 => [
+    'type' => 'singleValue',
+    'title' => 'Anzahl der Tierarten in Wikidata',
+    'query' => <<<SPARQL
+SELECT (COUNT(?item) as ?count)
+WHERE {
+  ?item wdt:P105 wd:Q7432.
+}
+SPARQL
+  ],
+
+
+  25 => [
+    'type' => 'singleValue',
+    'title' => 'Anzahl der Personen in Wikidata',
+    'query' => <<<SPARQL
+SELECT (COUNT(?item) as ?count)
+WHERE {
+  ?item wdt:P31 wd:Q5.
+}
+SPARQL
+  ],
+
+
+  26 => [
+    'type' => 'images',
+    'title' => 'Benannt nach Nelson Mandela',
+    'query' => <<<SPARQL
+SELECT ?label ?image ?loc WHERE {
+  ?item wdt:P138 wd:Q8023.
+  ?item wdt:P18 ?image.
+  ?item wdt:P131 ?location.
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "de,en" . 
+    ?item rdfs:label ?label.
+    ?location rdfs:label ?loc.
+  }
+}
+ORDER BY MD5(CONCAT(STR(NOW()), STR(?item)))
+SPARQL
+  ],
+
+
+  27 => [
+    'type' => 'embed',
+    'title' => 'Herkunft von Nobelpreisträgern',
+    'query' => <<<SPARQL
+#defaultView:BubbleChart
+SELECT ?countryLabel (COUNT(?person) AS ?Preisträger)
+WHERE
+{
+  ?person wdt:P166/wdt:P31? wd:Q7191;
+          wdt:P19 ?place.
+  ?place wdt:P17 ?country.
+  SERVICE wikibase:label {
+  bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" .
+  }
+}
+GROUP BY ?countryLabel
+ORDER BY DESC(?Preisträger)
+SPARQL
+  ],
+
+
+];
